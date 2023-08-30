1d796fe4843ac010d7f769c54b8b3f21526723b7
Sauljr144 <saulsuazojr06@gmail.com>
2023-08-29 19:40:18 -0700

    added models code

--- Services/Context/DataContext.cs
+++ Services/Context/DataContext.cs
@@ -2,16 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogBackEnd.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogBackEnd.Services.Context
 {
+    // we need this code first to add our tabels to the server
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) : base(options){
-
-            
-
+        public DbSet<UserModel> UserInfo { get; set; }
+        public DbSet<BlogItemModel> BlogInfo { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        //function to create the tables themselves
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
